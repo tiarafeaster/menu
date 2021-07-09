5600d51039e7937312a37415056ea5cdba476f6e
Tiara Feaster <tfeaster21@gmail.com>
2021-07-09 13:48:00 -0400

    added images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
   return (
     <main>
-      <div className="container">hey ya'll</div>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+          <Categories />
+          <Menu items={menuItems} />
+        </div>
+      </section>
     </main>
   );
 }
